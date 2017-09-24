6d6a6002f3857e35e61c8504c049d9e8a84d6837
ngedwin <ngedwin98@gmail.com>
2017-09-24 00:58:49 -0700

    In HDF5Util.jl: Update method signatures to use Unions

--- src/HDF5Util.jl
+++ src/HDF5Util.jl
@@ -5,9 +5,9 @@ export getindex, hdf5tree, attributes
 using HDF5
 import Base: getindex
 
-getindex(obj::HDF5.DataFile, ind::Int) = obj[names(obj)[ind]]
+getindex(obj::Union{HDF5File,HDF5Group}, ind::Int) = obj[names(obj)[ind]]
 
-function hdf5tree(obj::HDF5.DataFile, level::Int=0)
+function hdf5tree(obj::Union{HDF5File,HDF5Group}, level::Int=0)
     for (j,n) in enumerate(names(obj))
         println(repeat("  ",level) * n * " [$(j)]")
         hdf5tree(obj[n],level+1)
@@ -15,7 +15,7 @@ function hdf5tree(obj::HDF5.DataFile, level::Int=0)
 end
 hdf5tree(obj::HDF5Dataset, level::Int) = return
 
-function atttributes(obj::HDF5Object)
+function atttributes(obj::Union{HDF5File, HDF5Group, HDF5Dataset})
     return Dict(n => read(attrs(obj)[n]) for n in names(attrs(obj)))
 end
 
