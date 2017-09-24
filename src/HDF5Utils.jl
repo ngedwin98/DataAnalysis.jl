9ee56f93eb5b1c03ad14bc141b6983df44036ec8
ngedwin <ngedwin98@gmail.com>
2017-09-24 04:03:00 -0700

    Move DataProcessingUtils.jl from /src to /

---
+++ src/HDF5Utils.jl
@@ -0,0 +1,22 @@
+module HDF5Utils
+
+export getindex, hdf5tree, attributes
+
+using HDF5
+import Base: getindex
+
+getindex(obj::Union{HDF5File,HDF5Group}, ind::Int) = obj[names(obj)[ind]]
+
+function hdf5tree(obj::Union{HDF5File,HDF5Group}, level::Int=0)
+    for (j,n) in enumerate(names(obj))
+        println(repeat("  ",level) * n * " [$(j)]")
+        hdf5tree(obj[n],level+1)
+    end
+end
+hdf5tree(obj::HDF5Dataset, level::Int) = return
+
+function atttributes(obj::Union{HDF5File, HDF5Group, HDF5Dataset})
+    return Dict(n => read(attrs(obj)[n]) for n in names(attrs(obj)))
+end
+
+end # module
